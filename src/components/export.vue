f0fa0ceae92c7c0dc3e9e60ee10095d3d759daa5
SHINING <jack7770126@gmail.com>
2017-11-29 19:15:25 +0800

    [dist] update dist.

--- src/components/export.vue
+++ src/components/export.vue
@@ -73,6 +73,29 @@
 </template>
 
 <script>
+Date.prototype.dateFormat = function(fmt) {
+  var o = {
+    "M+": this.getMonth() + 1, //月份
+    "d+": this.getDate(), //日
+    "h+": this.getHours(), //小时
+    "m+": this.getMinutes(), //分
+    "s+": this.getSeconds(), //秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+    S: this.getMilliseconds() //毫秒
+  };
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(
+      RegExp.$1,
+      (this.getFullYear() + "").substr(4 - RegExp.$1.length)
+    );
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt))
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length)
+      );
+  return fmt;
+};
 export default {
   data() {
     return {
@@ -173,36 +196,15 @@ export default {
   },
 
   methods: {
-    dateFormat(fmt) {
-      var o = {
-        "M+": this.getMonth() + 1, //月份
-        "d+": this.getDate(), //日
-        "h+": this.getHours(), //小时
-        "m+": this.getMinutes(), //分
-        "s+": this.getSeconds(), //秒
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-        S: this.getMilliseconds() //毫秒
-      };
-      if (/(y+)/.test(fmt))
-        fmt = fmt.replace(
-          RegExp.$1,
-          (this.getFullYear() + "").substr(4 - RegExp.$1.length)
-        );
-      for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt))
-          fmt = fmt.replace(
-            RegExp.$1,
-            RegExp.$1.length == 1
-              ? o[k]
-              : ("00" + o[k]).substr(("" + o[k]).length)
-          );
-      return fmt;
-    },
     reservaExport() {
       let getData = this.reservaForm;
       if (getData.dateRange !== "") {
-        getData.start = this.dateFormat(getData.dateRange[0]);
-        getData.end = this.dateFormat(getData.dateRange[1]);
+        getData.start = new Date(getData.dateRange[0]).dateFormat(
+          "yyyy-MM-dd HH:mm:ss"
+        );
+        getData.end = new Date(getData.dateRange[1]).dateFormat(
+          "yyyy-MM-dd HH:mm:ss"
+        );
       }
       let url = `${this.$api.host}output?table=reverse&start=${getData.start ||
         ""}&end=${getData.end || ""}&state=${getData.state}`;
@@ -213,8 +215,12 @@ export default {
     orderExport() {
       let getData = this.orderForm;
       if (getData.dateRange !== "") {
-        getData.start = this.dateFormat(getData.dateRange[0]);
-        getData.end = this.dateFormat(getData.dateRange[1]);
+        getData.start = new Date(getData.dateRange[0]).dateFormat(
+          "yyyy-MM-dd HH:mm:ss"
+        );
+        getData.end = new Date(getData.dateRange[1]).dateFormat(
+          "yyyy-MM-dd HH:mm:ss"
+        );
       }
       let url = `${this.$api.host}output?table=order&start=${getData.start ||
         ""}&end=${getData.end || ""}&state=${getData.state}`;
@@ -224,8 +230,12 @@ export default {
     masterExport() {
       let getData = this.masterForm;
       if (getData.dateRange !== "") {
-        getData.start = this.dateFormat(getData.dateRange[0]);
-        getData.end = this.dateFormat(getData.dateRange[1]);
+        getData.start = new Date(getData.dateRange[0]).dateFormat(
+          "yyyy-MM-dd HH:mm:ss"
+        );
+        getData.end = new Date(getData.dateRange[1]).dateFormat(
+          "yyyy-MM-dd HH:mm:ss"
+        );
       }
       let url = `${this.$api.host}output?table=worker&start=${getData.start ||
         ""}&end=${getData.end || ""}&state=${getData.state}`;
